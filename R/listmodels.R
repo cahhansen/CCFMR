8cba2d62c9bb2a7ca021fa06081c7eb0c6c2213a
cahhansen <carlyahansen@gmail.com>
2016-12-15 14:55:03 -0700

    Create listmodels function

---
+++ R/listmodels.R
@@ -0,0 +1,13 @@
+#' Shows available models for a particular dataset
+#'
+#' @param data data.frame with formatted time series of projections for various models
+#' @return list list of model names
+#'
+#' @export
+#'
+
+
+listmodels=function(data){
+  names=names(data)[2:length(names(data))]
+  print(names)
+}
