1fcc06ede92004bcb9dc4c3c4b9d718b65af6596
Carly Hansen <carlyahansen@gmail.com>
2016-12-15 22:11:23 -0700

    Create avgdaily function

---
+++ R/avgdaily.R
@@ -0,0 +1,18 @@
+#' Calculate Precipitation by Day of the Year
+#'
+#' @param data data.frame with the date and daily precipitation projections
+#'
+#' @return data.frame Data frame of averaged precipitation values
+#' @export
+#'
+#'
+
+avgdaily=function(data){
+  library(lubridate)
+  data$DOY=as.factor(yday(data$Date))
+  avgdata=tapply(data$Model,data$DOY,mean)
+  avgdata=data.frame("DOY"=c(seq(1,366)),"Avg"=avgdata)
+  return(avgdata)
+}
+
+
