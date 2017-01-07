1161bc26ea092e45f46fef89b1d3d86ee09eee28
Carly Hansen <carlyahansen@gmail.com>
2017-01-06 17:02:41 -0700

    Calculate different extreme percentiles

--- R/precipsummary.R
+++ R/precipsummary.R
@@ -11,5 +11,8 @@ precipsummary=function(data,precipcol){
   psum=summary(precip)
   pevents=length(precip[precip>0])
   print(psum)
+  print(quantile(precip,0.9))
+  print(quantile(precip,0.95))
+  print(quantile(precip,0.99))
   print(pevents)
 }
